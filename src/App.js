0b2d71393aa00e466cd1e01fafc8ed9fd6d805ba
gionuc-ai <jaconuc@gmail.com>
2025-11-10 10:25:27 +0100

    fix new build

--- src/App.js
+++ src/App.js
@@ -2233,11 +2233,8 @@ if (editingEvent) {
           </div>
         </div>
       )}
-    </div>
-  );
-};
 
-{/* Modal Telefono */}
+      {/* Modal Telefono */}
       {showPhoneAuth && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center p-4 z-50">
           <div className="bg-white rounded-2xl shadow-xl p-8 max-w-md w-full">
@@ -2334,5 +2331,8 @@ if (editingEvent) {
           </div>
         </div>
       )}
+    </div>
+  );
+};
 
 export default CalendarioAI;
